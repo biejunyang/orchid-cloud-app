3139e78be766f5d264d6f5f897840af15adddb29
biejunyang <biejunyang@cngb.org>
2020-11-27 17:31:42 +0800

    xxx

--- orchid-gateway/src/main/java/com/orchid/gateway/GatewayApp.java
+++ orchid-gateway/src/main/java/com/orchid/gateway/GatewayApp.java
@@ -3,6 +3,8 @@ package com.orchid.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -12,5 +14,17 @@ public class GatewayApp {
 
     public static void main(String[] args){
         SpringApplication.run(GatewayApp.class, args);
+    }
+
+
+    /**
+     * 限流熔断回退
+     */
+    @RestController
+    public class FallbackController {
+        @GetMapping("/fallback")
+        public String fallback() {
+            return "fallback";
+        }
     }
 }
